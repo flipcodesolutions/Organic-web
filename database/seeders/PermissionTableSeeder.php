6bbbc45616224eebc6cbea5d44facd620b66618c
unknown <rjjadav7773@gmail.com>
2025-02-12 14:30:14 +0530

    changes

--- database/seeders/PermissionTableSeeder.php
+++ database/seeders/PermissionTableSeeder.php
@@ -1,11 +1,11 @@
 <?php
 
 namespace Database\Seeders;
-  
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
-  
+
 class PermissionTableSeeder extends Seeder
 {
     /**
@@ -19,7 +19,7 @@ class PermissionTableSeeder extends Seeder
            'role-edit',
            'role-delete',
         ];
-        
+
         foreach ($permissions as $permission) {
              Permission::create(['name' => $permission]);
         }
