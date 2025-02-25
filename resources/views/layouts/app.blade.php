7d4e7b2b38d82db6961817c7996595da3bb2d224
Parvez Memon <137087093+Parvez2003@users.noreply.github.com>
2025-02-25 17:22:35 +0530

    changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
 
     <script src="{{ asset('js/script.js') }}"></script>
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
 
