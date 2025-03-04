812ac384ccd15dffdad53ea20d0bb234881927c4
Parvez Memon <137087093+Parvez2003@users.noreply.github.com>
2025-03-04 12:54:06 +0530

    api and migration changes

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->enum('is_verify_phone', ['yes', 'no'])->default('no')->nullable();
             $table->enum('status', ['active', 'deactive', 'deleted'])->default('active')->nullable();
 
-            $table->enum('is_special', ['yes', 'no'])->default('yes')->nullable();
+            $table->enum('is_special', ['yes', 'no'])->default('no')->nullable();
 
             $table->string('default_language')->nullable();
             $table->rememberToken();
