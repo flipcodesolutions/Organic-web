71d0b4f8444677c40b0bb00602cfd6cc8c864608
Parvez Memon <137087093+Parvez2003@users.noreply.github.com>
2025-03-08 15:49:21 +0530

    api changes

--- app/Http/Controllers/api/customer/ProductController.php
+++ app/Http/Controllers/api/customer/ProductController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\api\customer;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Product;
 use App\Utils\Util;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
@@ -16,17 +18,52 @@ class ProductController extends Controller
     public function products(Request $request)
     {
         try {
+            $validator = Validator::make($request->all(), [
+                'category_id' => 'required',
+            ]);
+
+            if ($validator->fails()) {
+                return Util::getErrorMessage('validation failed', $validator->errors());
+            }
             $language = Auth::user()->default_language;
+
             $currentPage = $request->input('page', 1);
+
             $search = $request->input('search');
+
             $productEnglishFields = ['*', 'productName as displayName', 'productDescription as displayDescription'];
             $productGujaratiFields = ['*', 'productNameGUj as displayName', 'productDescriptionGuj as displayDescription'];
             $productHindiFields = ['*', 'productNameHin as displayName', 'productDescriptionHin as displayDescription'];
 
-            // Start query with relationships
-            $query = Product::with(['productImages', 'productUnit.unitMaster'])
-                ->where('status', 'active')
-                ->select($language == 'Hindi' ? $productHindiFields : ($language == 'Gujarati' ? $productGujaratiFields : $productEnglishFields));
+            $categoriesEnglishFields = ['*', 'categoryName as displayName', 'categoryDescription as displayDescription'];
+            $categoriesGujaratiFields = ['*', 'categoryNameGUj as displayName', 'categoryDescriptionGuj as displayDescription'];
+            $categoriesHindiFields = ['*', 'categoryNameHin as displayName', 'categoryDescriptionHin as displayDescription'];
+
+            $childEnglishFields = ['categories.*', 'categoryName as displayName', 'categoryDescription as displayDescription'];
+            $childGujaratiFields = ['categories.*', 'categoryNameGUj as displayName', 'categoryDescriptionGuj as displayDescription'];
+            $childHindiFields = ['categories.*', 'categoryNameHin as displayName', 'categoryDescriptionHin as displayDescription'];
+
+
+            $categories = Category::where('id', $request->category_id)
+                ->with('childs', function ($query) use ($language, $childEnglishFields, $childGujaratiFields, $childHindiFields) {
+                    $query->select($language == 'Hindi' ? $childHindiFields : ($language == 'Gujarati' ? $childGujaratiFields : $childEnglishFields));
+                })
+                ->select($language == 'Hindi' ? $categoriesHindiFields : ($language == 'Gujarati' ? $categoriesGujaratiFields : $categoriesEnglishFields))
+                ->first();
+
+            if ($categories->parent_category_id == 0) {
+                $childCategoryIds = $categories->childs->pluck('id')->toArray();
+                $query = Product::with(['productImages', 'productUnit.unitMaster'])
+                    ->where('status', 'active')
+                    ->where('categoryId', $childCategoryIds)
+                    ->select($language == 'Hindi' ? $productHindiFields : ($language == 'Gujarati' ? $productGujaratiFields : $productEnglishFields));
+            } else {
+                $query = Product::with(['productImages', 'productUnit.unitMaster'])
+                    ->where('status', 'active')
+                    ->where('categoryId', $request->category_id)
+                    ->select($language == 'Hindi' ? $productHindiFields : ($language == 'Gujarati' ? $productGujaratiFields : $productEnglishFields));
+            }
+
 
             if (!empty($search)) {
                 $query->where(function ($q) use ($search) {
@@ -38,6 +75,7 @@ class ProductController extends Controller
 
             $productsQuery->getCollection()->transform(function ($product) {
                 $product->productUnit->transform(function ($unit) {
+                    if (!isset($unit->unitMaster)) return $unit;
                     $unit->unit = $unit->unitMaster->unit;
                     unset($unit->unitMaster);
                     return $unit;
@@ -45,59 +83,10 @@ class ProductController extends Controller
                 return $product;
             });
 
-            return Util::getSuccessMessage('Products', $productsQuery);
+            return Util::getSuccessMessage('Products', ['categories' => $categories, 'products' => $productsQuery]);
         } catch (\Exception $e) {
             return Util::getErrorMessage('Something went wrong', ['error' => $e->getMessage()]);
         }
     }
 
-
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
