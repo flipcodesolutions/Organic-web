ddd6ca72981ff2ec7848dc7dd3e330ddc6426f8b
hardik <hpatel3880@gmail.com>
2025-03-17 11:07:07 +0530

    changes in app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,7 @@
     <title>Vegitable e-comm</title>
 
     <!-- Custom fonts for this template-->
-    <link href="{{ asset('asset/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('asset/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
